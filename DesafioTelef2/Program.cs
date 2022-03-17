01d73dddc0388560a9a159ba2874cffe817686b3
cesar antonio cuitiño <cesar_ant@live.com.ar>
2022-03-17 13:11:45 -0300

    Segunda subida

---
+++ DesafioTelef2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioTelef2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Ingrese modelo y marca: ");
+            string Mod = Console.ReadLine();
+            string Mar = Console.ReadLine();
+            Telefono t1 = new Telefono(Mod, Mar);
+            Console.WriteLine(" Ingrese el numero de tel: ");
+            t1.NumTele = Console.ReadLine();
+            Console.WriteLine(t1.llamar());
+            Console.WriteLine(" Ingresar contacto ");
+            string contac = Console.ReadLine();
+            Console.WriteLine(t1.llamar(contac));
+            Console.WriteLine(" Codigo de operador: ");
+            t1.CodOpe = int.Parse(Console.ReadLine());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesafioTelef2/Telefono.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioTelef2
+{
+    internal class Telefono
+    {
+        string modelo;
+        string marca;
+        string numTele;
+        int codOpe;
+
+        public string Modelo { get { return modelo; } }
+        public string Marca { get { return marca; } }
+        public string NumTele { get { return numTele; } set { numTele = value; } }
+        public int CodOpe 
+        { 
+            get { return codOpe; } 
+            set 
+            {
+                if (value > 0 || value < 3)
+                { codOpe = value; }
+                else { codOpe = 0; }
+            } 
+        }
+
+        public Telefono(string Modelo, string Marca)
+        {
+            this.modelo = Modelo;
+            this.marca = Marca;
+        }
+        public string llamar()
+        {
+            return " Realizando llamada.. ";
+        }
+        public string llamar(string contacto)
+        {
+            return " Llamando a " + contacto;
+        }
+        
+        
+    }
+}
